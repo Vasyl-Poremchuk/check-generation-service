70c48c37e78acd2822e5fd2372d85d80ff96b32e
Vasyl Poremchuk <nleveryone@gmail.com>
2023-03-27 19:29:21 +0300

    Registration of models in the admin site

--- check_service/admin.py
+++ check_service/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from check_service.models import Printer, Check
+
+
+@admin.register(Printer)
+class PrinterAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("name", "check_type")
+
+
+@admin.register(Check)
+class CheckAdmin(admin.ModelAdmin):
+    list_filter = ("printer_id", "check_type", "status")
